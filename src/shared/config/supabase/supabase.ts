82fb938682625983f318e2e06533a4bbd6ba4119
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-05-24 18:34:59 +0600

    chore: define supabase types

--- src/shared/config/supabase/supabase.ts
+++ src/shared/config/supabase/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+import { Database } from '@/shared/config/supabase/types';
 
 const supabaseUrl = process.env.VUE_APP_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.VUE_APP_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
