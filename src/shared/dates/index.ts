8beae7f9967a6da9e92839fe92294f1eb001cd7c
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-05-24 14:56:06 +0600

    chore: small updates

--- src/shared/dates/index.ts
+++ src/shared/dates/index.ts
@@ -4,17 +4,23 @@ interface MonthDates {
   startDate: string
   endDate: string
 }
-export function getCurrentMonthDates(): MonthDates {
+const getCurrentMonthDates = (): MonthDates => {
   const startDate = dayjs().startOf('month').format()
   const endDate = dayjs().endOf('month').format()
 
   return { startDate, endDate }
 }
 
-export function formatDateByTemplate(template: string): string {
+const formatDateByTemplate = (template: string): string => {
   return dayjs().format(template)
 }
 
-export const formatDate = (date: string): string => {
+const formatDate = (date: string): string => {
   return dayjs(date).format('DD.MM.YYYY')
+}
+
+export default {
+  getCurrentMonthDates,
+  formatDateByTemplate,
+  formatDate,
 }
