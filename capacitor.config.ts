0094a1ea7a333883eb8e0b5974e51e169e03b5ca
andrei.detenkov <a.detenkov@gmail.com>
2024-08-04 11:46:20 +0600

    chore: creating app release

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.budget.app',
-  appName: 'budget-app',
+  appName: 'budget',
   webDir: 'dist',
 };
 
