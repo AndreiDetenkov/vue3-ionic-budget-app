5dcde1f285c9d36cfe74e8d5fc5b6441390d4a03
andrei.detenkov <a.detenkov@gmail.com>
2024-08-04 15:23:43 +0600

    feat: add alert before removing

--- src/entities/transactions/ui/TransactionListItemSlide.vue
+++ src/entities/transactions/ui/TransactionListItemSlide.vue
@@ -1,23 +1,51 @@
 <script setup lang="ts">
-import { IonItemSliding, IonItemOptions, IonItemOption, IonIcon, IonLabel } from '@ionic/vue';
+import { ref } from 'vue';
+import { IonItemSliding, IonItemOptions, IonItemOption, IonIcon, IonLabel, alertController } from '@ionic/vue';
 import { trash } from 'ionicons/icons';
-import { Transaction, TransactionListItem } from '@/entities/transactions';
+import { Transaction, TransactionListItem, useTransactionStore } from '@/entities/transactions';
 
 const prop = defineProps<{
   transaction: Transaction;
 }>();
 
-const emit = defineEmits<{
-  'remove-transaction': [id: string];
-}>();
+const itemSliding = ref();
+
+const store = useTransactionStore();
+const { removeTransaction } = store;
+
+const onRemoveHandler = () => {
+  removeTransaction(prop.transaction.id);
+  itemSliding.value?.$el.close();
+};
+
+const presentAlert = async () => {
+  const alert = await alertController.create({
+    subHeader: 'Submit your action',
+    message: `${prop.transaction.name} will be removed.`,
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => itemSliding.value?.$el.close(),
+      },
+      {
+        text: 'Remove',
+        role: 'confirm',
+        handler: () => onRemoveHandler(),
+      },
+    ],
+  });
+
+  await alert.present();
+};
 </script>
 
 <template>
-  <ion-item-sliding>
+  <ion-item-sliding ref="itemSliding">
     <transaction-list-item :transaction="transaction" />
 
-    <ion-item-options @click="emit('remove-transaction', prop.transaction.id)">
-      <ion-item-option color="danger">
+    <ion-item-options>
+      <ion-item-option @click.stop="presentAlert" color="danger">
         <ion-icon slot="start" :icon="trash"></ion-icon>
         <ion-label>Remove</ion-label>
       </ion-item-option>
