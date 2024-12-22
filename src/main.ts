18bf4e85cd22aff10867f7d5eddd3dbd9fc35bf5
andrei.detenkov <a.detenkov@gmail.com>
2024-12-22 22:26:49 +0600

    refactor: reorganize structure

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,28 @@
-import { app } from './app';
-import router from '@/app/router';
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { IonicVue } from '@ionic/vue';
+import router from '@/router';
+import App from '@/App.vue';
 
-router.isReady().then(() => {
-  app.mount('#app');
-});
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/vue/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/vue/css/padding.css';
+// import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/display.css';
+
+/* Theme variables */
+import '@/assets/theme/variables.css';
+
+const app = createApp(App).use(IonicVue).use(router).use(createPinia());
+
+router.isReady().then(() => app.mount('#app'));
