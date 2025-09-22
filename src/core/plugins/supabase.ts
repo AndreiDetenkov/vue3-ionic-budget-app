eb80141c74a336b72b8286777cf02de492c7596d
andrei.detenkov <a.detenkov@gmail.com>
2025-09-22 23:52:17 +0600

    refactor: rename Supabase API key env variable for consistency

--- src/core/plugins/supabase.ts
+++ src/core/plugins/supabase.ts
@@ -3,6 +3,6 @@ import { checkEnvVariable } from '@/core/utils/env';
 import type { Database } from '@/core/plugins/database.types';
 
 export const SUPABASE_URL = checkEnvVariable('VITE_SUPABASE_URL');
-export const API_KEY = checkEnvVariable('VITE_SUPABASE_ANON_KEY');
+export const API_KEY = checkEnvVariable('VITE_SUPABASE_API_KEY');
 
 export const supabase = createClient<Database>(SUPABASE_URL, API_KEY);
