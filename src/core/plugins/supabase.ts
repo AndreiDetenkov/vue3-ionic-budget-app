9eece9a39ddbf3b5692302363e8a3f361157d142
andrei.detenkov <a.detenkov@gmail.com>
2025-09-22 23:03:30 +0600

    refactor: extract database types into separate file and update supabase client setup

--- src/core/plugins/supabase.ts
+++ src/core/plugins/supabase.ts
@@ -1,52 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 import { checkEnvVariable } from '@/core/utils/env';
-
-interface Database {
-  public: {
-    Tables: {
-      categories: {
-        Row: {
-          icon: string;
-          id: string;
-          title: string;
-        };
-        Insert: {
-          icon?: string | null;
-          id: string;
-          title: string;
-        };
-        Update: {
-          icon?: string | null;
-          id?: string;
-          title?: string;
-        };
-      };
-      transactions: {
-        Row: {
-          category_id: string;
-          created_at: string;
-          id: string;
-          name: string;
-          value: number;
-        };
-        Insert: {
-          category_id?: string;
-          created_at?: string;
-          id?: string;
-          name?: string;
-          value: number;
-        };
-        Update: {
-          category_id?: string;
-          created_at?: string;
-          id?: string;
-          name?: string;
-          value?: number;
-        };
-      };
-    };
-  };
-}
+import type { Database } from '@/core/plugins/database.types';
 
 export const SUPABASE_URL = checkEnvVariable('VITE_SUPABASE_URL');
 export const API_KEY = checkEnvVariable('VITE_SUPABASE_ANON_KEY');
